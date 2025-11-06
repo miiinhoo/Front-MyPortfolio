b12903be40d61b1ed536f65ca9102f682ba68f8c
miiinhoo <alsgh4454@naver.com>
2025-11-06 22:11:43 +0900

    add : MainPage추가

---
+++ src/arrays/TitleArrays.jsx
@@ -0,0 +1,6 @@
+export const Title = [
+  ["Code meets design.", "Ideas become reality."],
+  ["Every pixel has a purpose.", "Less chaos, more flow."],
+  ["Design with intent.", "Build with clarity."],
+  ["Turning ideas into reality.", "One line of code at a time."],
+]
