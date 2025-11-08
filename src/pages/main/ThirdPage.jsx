64705d30af5dd33988e1be273db97d6f78fa7c1b
miiinhoo <alsgh4454@naver.com>
2025-11-08 21:14:40 +0900

    add : swiper추가하는 작업

--- src/pages/main/ThirdPage.jsx
+++ src/pages/main/ThirdPage.jsx
@@ -4,20 +4,30 @@ import ButtonComponent from "../../components/common/ButtonComponent";
 import "./Swiper.scss";
 
 export default function ThirdPage(){
-    const { bool,setBool } = useCustomHook();
+    const { bool,setBool, boolB: openSide, setBoolB:setOpenSide } = useCustomHook();
     return(
         <section>
             <div className="page-inner">
                 <div className="button-area">
-                    <ButtonComponent text={"Front"} 
-                    event={() => setBool(true)}
+                    <ButtonComponent text={"Front & Design"} 
+                    event={() => {setBool(true);setOpenSide(true)}}
                     cln={bool ? "bold":""}/>
+                    {bool ? (
+                        <div className={"floatBox"}>
+                            <ButtonComponent text={"Front"} 
+                            event={() => setOpenSide(true)}
+                            cln={openSide ? "bold":""}/>
+                            <ButtonComponent text={"Design"} 
+                            event={() => setOpenSide(false)}
+                            cln={!openSide ? "bold":""}/>
+                        </div>                        
+                        ):(<></>)}
                     <ButtonComponent text={"Team"} 
                     event={() => setBool(false)}
                     cln={!bool ? "bold":""}/>
                 </div>
                 
-                <SwiperPage bool={bool}/>
+                <SwiperPage bool={bool} openSide={openSide}/>
             </div>
         </section>
         
