bee4add459be20791588c5673d3f4916be16bb34
miiinhoo <alsgh4454@naver.com>
2025-11-19 22:19:11 +0900

    fix : Swiper 기능 변경 등 작업

--- src/imgs/projects/common/project.jsx
+++ src/imgs/projects/common/project.jsx
@@ -3,5 +3,6 @@ import Sherlock from '../sherlock.png';
 import MLB from '../mlb.png';
 import Vonin from '../vonin.png';
 import Paytime from '../paytime.PNG';
+import Wooden from '../wooden.JPG';
 
-export { Designer, Sherlock, MLB, Vonin,Paytime };
+export { Designer, Sherlock, MLB, Vonin,Paytime,Wooden };
