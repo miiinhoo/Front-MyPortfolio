885b6edeca75b25ff724ada7cb4472ffb5ea1bfe
miiinhoo <alsgh4454@naver.com>
2025-11-22 19:23:49 +0900

    add : Work 페이지들 추가

--- src/pages/about/work/WoodenPage.jsx
+++ src/pages/about/work/WoodenPage.jsx
@@ -1,7 +1,7 @@
 export default function WoodenPage() {
     return(
         <>
-        
+            wooden페이지
         </>
     )
 }
--- src/routers/AboutRouter.jsx
+++ src/routers/AboutRouter.jsx
@@ -1,5 +1,6 @@
 import { Navigate } from "react-router-dom";
 import { lazy, Suspense } from "react";
+import WorkRouter from "./WorkRouter";
 
 const loading = <div>로딩중..</div>;
 const Profile = lazy(() => import("../pages/about/ProfilePage"));
@@ -26,10 +27,11 @@ const AboutRouter = [
     path: "work",
     element: (
       <Suspense fallback={loading}>
-        <Work />
+        <Work/>
       </Suspense>
     ),
-  },
+    children: WorkRouter
+  },  
 ];
 
 export default AboutRouter;
